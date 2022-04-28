65bbb1cf98df8c847190b586f6dcea2fec769774
Daniel Ibarra <dih_nes@yahoo.com.mx>
2022-04-28 18:48:58 -0500

    Login page updated

--- ClientApp/src/components/Login.jsx
+++ ClientApp/src/components/Login.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { Button, Box, Icon } from '@chakra-ui/react'
+import { Button, Box, Icon, VStack, Center, Text, background } from '@chakra-ui/react'
 import { MdVpnKey } from 'react-icons/md'
+import icon from '../configs/icons'
 
 const Login = () => {
+
+  const hoverButtonStyle = {
+    background: 'purple.500',
+    color: 'white'
+  }
+
   return (
-    <Box>
-      <Icon as=''></Icon>
-      <Button leftIcon={<Icon as={MdVpnKey} />}>Sign in</Button>
-    </Box>
+    <Center w='100vw' h='100vh' bg='gray.200'>
+      <VStack px='150px' py='100px' borderRadius='10px' shadow='md' bg='white'>
+        <Icon fontSize={100} color='purple.600' as={icon.app_logo.outline}></Icon>
+        <Text fontSize={30}>EventNet</Text>
+        <Button _hover={hoverButtonStyle}
+          leftIcon={<Icon as={MdVpnKey} />}>Sign in</Button>
+      </VStack>
+    </Center>
   )
 }
 

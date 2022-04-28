4dc403930cb20ef249d93c62938db262b50ec1a0
cih2 <cih_dmc@yahoo.com.mx>
2022-04-27 20:55:02 -0500

    convert App to arrow function

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -9,16 +9,14 @@ import './custom.css'
 
 // este es un comentario
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+const App = () => {
+  return (
+    <Layout>
+      <Route exact path='/' component={Home} />
+      <Route path='/counter' component={Counter} />
+      <Route path='/fetch-data' component={FetchData} />
+    </Layout>
+  );
 }
+
+export default App

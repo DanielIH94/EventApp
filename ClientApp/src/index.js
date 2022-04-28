2e30991649de72a3ca44abafb9c68d0b88b13cee
cih2 <cih_dmc@yahoo.com.mx>
2022-04-27 22:32:13 -0500

    chakra-ui installed

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -6,14 +6,19 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from "@chakra-ui/react"
+
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>,
-  rootElement);
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

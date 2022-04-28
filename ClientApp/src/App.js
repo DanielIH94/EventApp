cbb0ebbc1b4e3e6c72717a40890d60567c695de4
cih2 <cih_dmc@yahoo.com.mx>
2022-04-27 21:54:10 -0500

    FIX : eslint issues

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import Home from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
 import './custom.css'
-
-// este es un comentario
 
 const App = () => {
   return (
